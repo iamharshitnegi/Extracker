2f0fed0be13d007a6d5ea43a5ae08c831c3a0ba4
Harshit <harshit.igen@gmail.com>
2023-10-24 01:17:38 +0530

    User login : validating username and email (part 1)

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
-from .views import RegistrationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
+from django.views.decorators.csrf import csrf_exempt
 from django.urls import path
 
 
 urlpatterns = [
-    path('register_page', RegistrationView.as_view(), name="register_page")
+    path('register_page', RegistrationView.as_view(), name="register_page"),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
+    path('validate-email',csrf_exempt(EmailValidationView.as_view()),name='validate-email')
 ]
