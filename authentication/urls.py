67de72185916ff4c4132b78a558fee9f1a57b8dd
Digeshwar Dewangan <digeshwar.dd786@gmail.com>
2023-11-26 05:16:11 +0530

    reset password functionality added

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView, LogoutView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView, LogoutView, RequestPasswordResetEmail, CompletePasswordReset
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import path
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
     path('validate-email',csrf_exempt(EmailValidationView.as_view()),name='validate-email'),
     path('activate/<uidb64>/<token>',VerificationView.as_view(),name="activate" ),
-    path('logout', LogoutView.as_view(), name="logout")
+    path('logout', LogoutView.as_view(), name="logout"),
+    path('request-reset-password', RequestPasswordResetEmail.as_view(), name="request-reset-password"),
+    path('set-new-password/<uidb64>/<token>',CompletePasswordReset.as_view(),name="set-new-password" ),
 ]
